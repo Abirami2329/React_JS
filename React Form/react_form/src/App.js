3c48cfec624dd3d70b74afd12faec874ddf31f71
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-05-03 20:40:37 +0530

    Update App.js

--- React Form/react_form/src/App.js
+++ React Form/react_form/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
-import './index.css'
+import "./index.css";
 
 function MyForm() {
   const [inputs, setInputs] = useState({});
@@ -8,40 +8,93 @@ function MyForm() {
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(inputs);
-    alert('You have submitted the form.');
-  }
+    alert("You have submitted the form.");
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-       <br></br>
-       <br></br>
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        <br></br>
-        <br></br>
-        </label>
-        <input type="submit" />
+      <div className="App-header">
+        <div className="">
+          <span>
+            <h3>REGISTRATION FORM</h3>
+          </span>
+        </div>
+        <div className="row">
+          <label className="label">Enter Name:</label>
+          <div className="column_input">
+            <input
+              type="text"
+              name="username"
+              value={inputs.username || ""}
+              onChange={handleChange}
+              style={{ marginBottom: 20 }}
+              className="input"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <label className="label">Enter DOB:</label>
+          <div className="column_input">
+            <input
+              type="date"
+              name="age"
+              value={inputs.age || ""}
+              onChange={handleChange}
+              style={{ marginBottom: 20 }}
+              className="input"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <label className="label">Enter Mobile Number:</label>
+          <div className="column_input">
+            <input
+              type="number"
+              name="mobile"
+              value={inputs.mobile || ""}
+              onChange={handleChange}
+              style={{ marginBottom: 20 }}
+              className="input"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <label className="label">Enter Email:</label>
+          <div className="column_input">
+            <input
+              type="text"
+              name="email"
+              value={inputs.email || ""}
+              onChange={handleChange}
+              style={{ marginBottom: 20 }}
+              className="input"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <label className="label">Enter Address:</label>
+          <div className="column_input">
+            <textarea
+              name="address"
+              value={inputs.address || ""}
+              onChange={handleChange}
+              style={{ marginBottom: 20 }}
+              className="input"
+            />
+          </div>
+        </div>
+        <div className = 'button_row'> 
+        <input type="submit" style={{ width: 120 }} className="button" />
+        </div>
+      </div>
     </form>
-  )
+  );
 }
 
 export default MyForm;
