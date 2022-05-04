1e314882797c2a6e016c68ee637189035cd53a92
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-05-04 23:48:40 +0530

    Update App.js

--- React Form/react_form/src/App.js
+++ React Form/react_form/src/App.js
@@ -5,6 +5,43 @@ import "./index.css";
 function MyForm() {
   const [inputs, setInputs] = useState({});
 
+  const validateName = (event) => {
+    if (document.getElementById("name").value) {
+      handleChange(event);
+    } else {
+      alert("Name shoule not be blank");
+    }
+  };
+  const validateDob = (event) => {
+    const emailValue = document.getElementById("dob").value;
+    if (emailValue) {
+      handleChange(event);
+    } else {
+      alert("Invalid Email");
+    }
+  };
+  const validateMobile = (event) => {
+    if (document.getElementById("mobile").value) {
+      handleChange(event);
+    } else {
+      alert("MobileNumber shoule not be blank");
+    }
+  };
+  const validateEmail = (event) => {
+    if (document.getElementById("email").value) {
+      handleChange(event);
+    } else {
+      alert("email shoule not be blank");
+    }
+  };
+  const validateAddress = (event) => {
+    if (document.getElementById("address").value) {
+      handleChange(event);
+    } else {
+      alert("Address shoule not be blank");
+    }
+  };
+
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -13,14 +50,22 @@ function MyForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(inputs);
-    alert("You have submitted the form.");
+    if (
+      inputs.username &&
+      inputs.age &&
+      inputs.mobile &&
+      inputs.email &&
+      inputs.address
+    ) {
+      alert("You have submitted the form.");
+    }
+    alert("Please Enter All Details");
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="App-header">
-        <div className="">
+        <div>
           <span>
             <h3>REGISTRATION FORM</h3>
           </span>
@@ -29,10 +74,11 @@ function MyForm() {
           <label className="label">Enter Name:</label>
           <div className="column_input">
             <input
+              id="name"
               type="text"
               name="username"
               value={inputs.username || ""}
-              onChange={handleChange}
+              onChange={validateName}
               style={{ marginBottom: 20 }}
               className="input"
             />
@@ -42,10 +88,11 @@ function MyForm() {
           <label className="label">Enter DOB:</label>
           <div className="column_input">
             <input
+              id="dob"
               type="date"
               name="age"
               value={inputs.age || ""}
-              onChange={handleChange}
+              onChange={validateDob}
               style={{ marginBottom: 20 }}
               className="input"
             />
@@ -55,10 +102,11 @@ function MyForm() {
           <label className="label">Enter Mobile Number:</label>
           <div className="column_input">
             <input
+              id="mobile"
               type="number"
               name="mobile"
               value={inputs.mobile || ""}
-              onChange={handleChange}
+              onChange={validateMobile}
               style={{ marginBottom: 20 }}
               className="input"
             />
@@ -68,10 +116,11 @@ function MyForm() {
           <label className="label">Enter Email:</label>
           <div className="column_input">
             <input
+              id="email"
               type="text"
               name="email"
               value={inputs.email || ""}
-              onChange={handleChange}
+              onChange={validateEmail}
               style={{ marginBottom: 20 }}
               className="input"
             />
@@ -81,16 +130,17 @@ function MyForm() {
           <label className="label">Enter Address:</label>
           <div className="column_input">
             <textarea
+              id="address"
               name="address"
               value={inputs.address || ""}
-              onChange={handleChange}
+              onChange={validateAddress}
               style={{ marginBottom: 20 }}
               className="input"
             />
           </div>
         </div>
-        <div className = 'button_row'> 
-        <input type="submit" style={{ width: 120 }} className="button" />
+        <div className="button_row">
+          <input type="submit" style={{ width: 120 }} className="button" />
         </div>
       </div>
     </form>
