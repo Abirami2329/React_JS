b666796baedb4260cf902ed9fe367e9b5c74cd55
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-05-06 00:36:21 +0530

    Update footer.js

--- react_redux_components/src/Components/footer.js
+++ react_redux_components/src/Components/footer.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "../index.css";
-import {useDispatch, useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 function Footer() {
-    const elementValue = useSelector((state) => state);
-    console.log(useSelector((state) => state))
-    const dispatch = useDispatch();
-    return (
-      <div id="Footer">
-        <footer className="footer">
-          <h2>Footer</h2>
-          <h1>{elementValue.value}</h1>
-        </footer>
-      </div>
-    );
-  }
-  export default Footer;
+  const elementValue = useSelector((state) => state);
+  console.log(useSelector((state) => state));
+  return (
+    <div id="Footer">
+      <footer className="footer">
+        <h2>Footer</h2>
+        <h1>{elementValue.value}</h1>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
