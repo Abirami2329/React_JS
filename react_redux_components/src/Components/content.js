f1757a4b61ce12cf63a47fe97b0a5dd47fc6e0d5
Abirami2329 <96733813+Abirami2329@users.noreply.github.com>
2022-05-06 00:37:41 +0530

    Update content.js

--- react_redux_components/src/Components/content.js
+++ react_redux_components/src/Components/content.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "../index.css";
-import { useDispatch, useSelector } from "react-redux";
-import { getElementValue} from "../index";
+import { useDispatch } from "react-redux";
+import { getElementValue } from "../index";
 function Content() {
-  const elementValue = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <div>
@@ -11,7 +10,12 @@ function Content() {
         <h1>CONTENT</h1>
         <input id="name" className="input" placeholder="Enter a Name"></input>
         <br></br>
-        <button className="submitbtn" onClick={() => dispatch(getElementValue())}>SUBMIT </button>
+        <button
+          className="submitbtn"
+          onClick={() => dispatch(getElementValue())}
+        >
+          SUBMIT{" "}
+        </button>
       </article>
     </div>
   );
